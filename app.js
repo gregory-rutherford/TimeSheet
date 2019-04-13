49383df82463222be2a0bf88b3ec7dafec8e55f2
Gregory Rutherford <gregorford@gmail.com>
2019-04-13 10:53:58 -0500

    skeleton files

---
+++ app.js
@@ -0,0 +1,12 @@
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyCh0SkGb9A6ieagW4FbIkPtFvm4ZxxBzGA",
+    authDomain: "timesheet-3d54d.firebaseapp.com",
+    databaseURL: "https://timesheet-3d54d.firebaseio.com",
+    projectId: "timesheet-3d54d",
+    storageBucket: "",
+    messagingSenderId: "486076353110"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
