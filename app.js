b0f91d0929ebf3e9c36f5305b1c44a108069bf06
Fred <fred1n@hotmail.com>
2019-04-13 12:09:44 -0500

    minor updates

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
- // Initialize Firebase
- var config = {
-    apiKey: "AIzaSyCh0SkGb9A6ieagW4FbIkPtFvm4ZxxBzGA",
-    authDomain: "timesheet-3d54d.firebaseapp.com",
-    databaseURL: "https://timesheet-3d54d.firebaseio.com",
-    projectId: "timesheet-3d54d",
-    storageBucket: "",
-    messagingSenderId: "486076353110"
-  };
-  firebase.initializeApp(config);
+// Initialize Firebase
+var config = {
+	apiKey: 'AIzaSyCh0SkGb9A6ieagW4FbIkPtFvm4ZxxBzGA',
+	authDomain: 'timesheet-3d54d.firebaseapp.com',
+	databaseURL: 'https://timesheet-3d54d.firebaseio.com',
+	projectId: 'timesheet-3d54d',
+	storageBucket: '',
+	messagingSenderId: '486076353110'
+};
+firebase.initializeApp(config);
 
-  var database = firebase.database();
+//firebase variable
+var database = firebase.database();
+
+//gathers user data from the from and store in variable
+$(document).on('click', '#add-user', function() {
+	event.preventDefault();
+	var name = $('#name-input').val().trim();
+	console.log(name);
+	var role = $('#role-input').val().trim();
+	console.log(role);
+	var startDate = $('#startDate-input').val().trim();
+	moment(startDate, 'M/D/YYYY').valueOf();
+	console.log(startDate);
+	var rate = $('#rate-input').val().trim();
+	console.log(rate);
+});
